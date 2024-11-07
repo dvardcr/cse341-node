54ce032cc3c2bb360a5932d2e3fea411b9ccf457
Diego Vargas <diego.vargasd@gmail.com>
2024-11-06 22:11:41 -0600

    Created Routes and Controllers - Organized structure

---
+++ controllers/mainController.js
@@ -0,0 +1,5 @@
+const main = (req, res) => {
+    res.send("Diego Vargas - Production");
+};
+
+module.exports = { main };
--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Diego Vargas");
-});
+app.use('/', require('./routes'));
 
 app.listen(process.env.PORT || port, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || port));
